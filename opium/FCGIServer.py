dd9eb0df35b8e819ce6617367661e1c2c7f0289a
Br. John Fn None <bjohnfn@gmail.com>
2019-11-09 03:33:41 +0300

    pipe mutex

--- opium/FCGIServer.py
+++ opium/FCGIServer.py
@@ -1,11 +1,13 @@
 from flup.server.fcgi import WSGIServer
 import json
+import threading
 
 _PIPE = None
+_LOCK = threading.Lock()
 
 
 def handler(environ, start_response):
-    global _PIPE
+    global _PIPE, _LOCK
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except (ValueError):
@@ -16,8 +18,14 @@ def handler(environ, start_response):
     # in the file like wsgi.input environment variable.
     pack = environ['wsgi.input'].read(request_body_size)
     data = json.loads(pack)
-    _PIPE.send(data)
-    resp = json.dumps(_PIPE.recv())
+    _LOCK.acquire()
+    try:
+        _PIPE.send(data)
+        obj = _PIPE.recv()
+    except Exception as e:
+        print(f"Exception in pipe communication: {e}")
+    _LOCK.release()
+    resp = json.dumps(obj)
     status = '200 OK'
     response_headers = [('Content-type', 'application/json'), ('Content-Length', str(len(resp)))]
     start_response(status, response_headers)
